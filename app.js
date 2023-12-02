b74d1961f42c4f0af842584db85244ed7c716721
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2023-12-02 19:11:54 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const inputs = document.querySelectorAll(".input-field");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+let currentIndex = 0;
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
+});
+
+function moveSlider(index) {
+  currentIndex = index; // Update the currentIndex
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach((img) => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  bullets[index - 1].classList.add("active");
+}
+
+function autoSlide() {
+  currentIndex = (currentIndex % bullets.length) + 1; // Increment index cyclically
+  moveSlider(currentIndex);
+}
+
+const slideInterval = setInterval(autoSlide, 5000);
+
+bullets.forEach((bullet, index) => {
+  bullet.addEventListener("click", () => moveSlider(index + 1));
+});
+
+autoSlide();
